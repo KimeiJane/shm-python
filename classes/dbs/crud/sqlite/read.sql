25d4407090d6b654365870dbbdb7325c62f76350
Jane Nduku Kimei <Janetnduku519@gmail.com>
2025-08-18 11:29:07 +0300

    shm

--- classes/dbs/crud/sqlite/read.sql
+++ classes/dbs/crud/sqlite/read.sql
@@ -1,8 +1,24 @@
+--SELECT <columns // *>
+--FROM <table>
+-- WHERE <READ CONSTRAINS>
+--ORDER <>  LIMIT <>
+
+--Read all columns
+
+--SELECT * FROM student;
 
 
+--Read only name and email
 
+--SELECT name, email FROM student
 
+--read using where (specific)
 
+--SELECT * FROM  student
+--WHERE name = 'Akothee'
+--WHERE pocket_money>50
+--AND pocket_money<1000
 
-
-
+SELECT * FROM student 
+ORDER BY name ASC
+LIMIT 2
---
+++ classes/dbs/crud/sqlite/update.sql
@@ -0,0 +1,17 @@
+--update <table_name>
+--SET <columns to update>
+--where  <constrain>
+
+UPDATE student
+SET name ='AWINJA'
+WHERE id =1;
+
+--marks=0
+--WHERE marks IS NULL;
+
+--SELECT name,marks
+--FROM student
+
+UPDATE student
+SET email='kish@email.com'
+WHERE email='kish@email.com'
