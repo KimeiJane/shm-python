cb0f3688f3516a35d2f09d97c15f49c70a0acbde
Jane Nduku Kimei <Janetnduku519@gmail.com>
2025-08-18 10:52:55 +0300

    shm-python

---
+++ classes/dbs/crud/postgresql/create.sql
@@ -0,0 +1,8 @@
+--INSERT INTO <table name>
+--(<column>) VALUES (),(),();
+
+INSERT INTO student
+(name,email,phone,dob)
+VALUES
+('Kish','mwala@gmail.com' 1234567,'3.11.24')
+('Bree','bree@gmail.com' 98765,'4.02.23')
